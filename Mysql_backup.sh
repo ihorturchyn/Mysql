8d135c6894b53b30ba7c2aa46e8ec6f6aa79ee84
ihorturchyn <61976424+ihorturchyn@users.noreply.github.com>
2020-03-31 16:41:47 +0300

    Update Mysql_backup.sh

--- Mysql_backup.sh
+++ Mysql_backup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-DATE=$(date +%m-%d-%Y)
-BACKUP_DIR="/home/igorturchin"
-LOG_FILE="/home/igorturchin/Scripts/NewScripts/Log_file"
-#добавил проверку запущенного MysqlD. Если он не запущен - то и бекапировать нечего будет. В будущем добавлю отдельную процедуру для запуска mysql 
-#если таковой не будет запущен.
+DATE=$(date +%d-%m-%Y-%H-%M-%S)
+BACKUP_DIR="/home/ihorturchyn"
+LOG_FILE="/home/ihorturchyn/Scripts/Log_file"
 Process=mysqld
 Checking=`ps -A | grep $Process`
-if [ "${Checking:-null}" = null ]; then
-    echo "Process Mysqld is not running at $DATE" >> $LOG_FILE
-else
+function backuping() {
     souce_backup=$(mysqldump --user=root --password=S9DgmH}d --result-file=$BACKUP_DIR/Backup_$DATE.sql --all-databases)
     if [ -f "$BACKUP_DIR/Backup_$DATE.sql" ]; then
-        chmod 777 "$BACKUP_DIR/Backup_$DATE.sql"
+        chmod +x "$BACKUP_DIR/Backup_$DATE.sql"
         tar -zcvpf $BACKUP_DIR/Backup_$DATE.sql.tar.gz $BACKUP_DIR/Backup_$DATE.sql
         echo "At $DATE Backup was created $BACKUP_DIR/Backup_$DATE.sql.tar.gz" >> $LOG_FILE
-        echo "OK"
     else
         echo "Backup $BACKUP_DIR/Backup_$DATE.sql.tar.gz was not created" >> $LOG_FILE
     fi
+}
+if [ "${Checking:-null}" = null ]; then
+    echo "Process Mysqld is not running at $DATE" >> $LOG_FILE
+    Running=`service mysql restart`                               #starting service mysql when it was not runned before
+        if [ $? -eq 0 ]; then
+            backuping
+        else
+            echo "Process mysql can not be runned at $DATE"
+        fi
+else
+    backuping
 fi
+
